dd97a5e3fd1a58b3352fda9c0cb2ec9596d2bb1c
NorthWood69 <3303727@gmail.com>
2019-04-23 15:33:05 +0300

    Second commit.

---
+++ hello1.py
@@ -0,0 +1,11 @@
+def hello_app(environ, start_response):
+    q_string = environ.get('QUERY_STRING')
+    pairs_list = q_string.split("&")
+    body = ""
+    for pair in pairs_list:
+        body += pair + "\n"
+
+    response_string = [('Content-type', 'text/plain')]
+
+    start_response('200 OK', response_string)
+    return [body.strip()]
