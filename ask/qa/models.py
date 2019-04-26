59a9a76451b9d6a86de35d6ff227aab099b91937
root <root@web.smb-it.local>
2019-04-26 23:09:15 +0300

    Задание 2.3.10

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,16 +7,16 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(default="", max_length=1024)
     text = models.TextField(default="")
-    added_at = models.DateField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
+    likes = models.ManyToManyField(User, related_name='question_like_user')
 
-    class QuestionManager(models.Manager):
-        def new(self):
-            return self.order_by('-added_at')
-        def popular(self):
-            return self.order_by('-rating')
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
 
 class Answer(models.Model):
     text = models.TextField(default="")
