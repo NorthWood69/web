d58e3dd3c7d06de09d76084789622a4c2ac1f293
root <root@web.smb-it.local>
2019-04-29 11:43:27 +0300

    Задание 2.3.10

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,7 +13,7 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(default="", max_length=1024)
     text = models.TextField(default="")
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_like_user')
