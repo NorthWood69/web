b420969387d2a4c50e7807b08d1100cf2ad72e2a
root <root@web.smb-it.local>
2019-04-30 01:01:21 +0300

    Задание 2.5.8

--- init.sh
+++ init.sh
@@ -5,14 +5,14 @@ sudo gunicorn -c /etc/gunicorn.d/django.py ask.wsgi:application -D
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
-#sudo /etc/init.d/mysql start
-sudo systemctl start mariadb
-mysql -uroot -e "DROP DATABASE IF EXISTS askappdb;"
-mysql -uroot -e "DROP USER 'askapp'@'localhost';" mysql
+sudo /etc/init.d/mysql start
+#sudo systemctl start mariadb
+#mysql -uroot -e "DROP DATABASE IF EXISTS askappdb;"
+#mysql -uroot -e "DROP USER 'askapp'@'localhost';" mysql
 mysql -uroot -e "CREATE DATABASE askappdb;"
 mysql -uroot -e "CREATE USER 'askapp'@'localhost' IDENTIFIED BY 'passwrd123';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON askappdb.* TO 'askapp'@'localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
 
-#sudo python ~/web/ask/manage.py makemigrations qa
-#sudo python ~/web/ask/manage.py migrate
+sudo python ~/web/ask/manage.py makemigrations qa
+sudo python ~/web/ask/manage.py migrate
--- mysql.sh
+++
@@ -1,8 +0,0 @@
-sudo /etc/init.d/mysql start
-#sudo systemctl start mariadb
-#mysql -uroot -e "DROP DATABASE IF EXISTS askappdb;"
-#mysql -uroot -e "DROP USER 'askapp'@'localhost';" mysql
-mysql -uroot -e "CREATE DATABASE askappdb;"
-mysql -uroot -e "CREATE USER 'askapp'@'localhost' IDENTIFIED BY 'passwrd123';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON askappdb.* TO 'askapp'@'localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
